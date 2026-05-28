35f74c86812a7ddece70c8e86d6c64a95709d146
devAlissonOliveira <devalissonoliveira@gmail.com>
2026-05-28 09:21:19 -0300

    Criado o arquivo ex01, referente ao exercício 01 da lista 3 do módulo 1, com a resolução do mesmo.

---
+++ ex01.js
@@ -0,0 +1,21 @@
+/*
+    1. Crie um objeto representando um produto com as propriedades: nome, preço,
+    categoria e quantidade em estoque. Use for...in para percorrer e exibir todas as
+    propriedades e seus valores. Em seguida, adicione uma nova propriedade
+    desconto ao objeto e exiba o preço final calculado.
+*/
+
+let produto = {
+    nome: "Jeep Compass",
+    preço: 150000,
+    categoria: "SUV",
+    qtdEstoque: 7
+}
+
+for(let props in produto){
+    console.log(`${props}: ${produto[props]}`);
+}
+
+produto.desconto = 30000;
+
+console.log(`Preço Final: ${produto.preço - produto.desconto}`);
